03abdc1075fc54644ec83165878483226ccc41a4
jperamak <jussi.peramaki@gmail.com>
2013-01-18 14:08:26 +0200

    Spec fixes

--- spec/models/url_slide_spec.rb
+++ spec/models/url_slide_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe UrlSlide do
   it "should have Slide as model" do
     UrlSlide.model_name.should eq("Slide")
--- spec/models/youtube_slide_spec.rb
+++ spec/models/youtube_slide_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe YoutubeSlide do
   it "should have Slide as model" do
     YoutubeSlide.model_name.should eq("Slide")
