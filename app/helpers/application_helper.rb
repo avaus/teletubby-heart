de665bd7fb092ad69589016263b1e1065e4ef847
jperamak <jussi.peramaki@gmail.com>
2013-01-24 09:43:08 +0200

    Use absolute urls for slide template assets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
     I18n.t word
   end
 
+  def absolute_asset_path(resource)
+    "#{request.protocol}#{request.host_with_port}#{asset_path resource}"
+  end
+
   include ActionView::Helpers::TagHelper
 end
