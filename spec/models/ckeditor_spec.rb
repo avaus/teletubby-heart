9585ceebe44863769120ee261b64462fe621443e
jperamak <jussi.peramaki@gmail.com>
2013-02-15 09:08:50 +0200

    false test fixed, coverage 100%

--- spec/models/ckeditor_spec.rb
+++ spec/models/ckeditor_spec.rb
@@ -9,8 +9,8 @@ describe Ckeditor do
   end
   describe Ckeditor::AttachmentFile do
     it "has valid url_thumb" do
-      ck = Ckeditor::Picture.new
-      ck.url_thumb.should == "/data/thumb/missing.png"
+      ck = Ckeditor::AttachmentFile.new
+      ck.url_thumb.should == "/assets/ckeditor/filebrowser/images/thumbs/unknown.gif"
     end
   end
 end
