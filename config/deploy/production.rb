74b901004402bafb5d9e8b6f2e8c0885e423e7d8
matiisi <matias.kantele@gmail.com>
2013-03-04 14:48:10 +0200

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 server "192.168.111.150", :app, :web, :db, :primary => true
 set :user, "teletubby_production"
-set :deploy_to, "/home/teletubby_production/project-teletubby-heart"
+set :deploy_to, "/home/teletubby_production/teletubby-heart"
 set :faye_port, "9290"
