5517a792ba7e37d15e341bc5e5b5de44fbe9e463
Matias Kantele <matias.kantele@gmail.com>
2013-03-08 16:05:27 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ TeletubbyHeart::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( channel.js client.js dashboard.js client_list.js)
+  config.assets.precompile += %w( channel.js client.js dashboard.js client_list.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
