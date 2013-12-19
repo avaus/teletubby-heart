59e9190c8f292212c7a24e2a4ab71c149610dc91
Siiseli <tino.myllyselka@gmail.com>
2013-12-19 10:56:47 +0200

    Test_server renamed to staging, configs now point to new production and staging addresses.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
-server "194.100.211.115", :app, :web, :db, :primary => true
+server "staging.avaustube.fi", :app, :web, :db, :primary => true
 set :user, "teletubby"
 set :deploy_to, "/home/teletubby/teletubby-heart"
 set :faye_port, "9292"
-set :rails_env, "test_server"
+set :rails_env, "staging"
